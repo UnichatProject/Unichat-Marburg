b936302a1b384212425752f460dfb5f815b10d06
UnichatProject <38441295+UnichatProject@users.noreply.github.com>
2018-04-18 01:36:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
 var url;
 var wsUrl;
 
-if (document.location.href.indexOf("localhost") !== -1) {
-    url = "http://localhost:4567/";
-    wsUrl = "ws://127.0.0.1:4567/";
-} else {
-    url = "https://cha-th.herokuapp.com/";
-    wsUrl = "wss://cha-th.herokuapp.com/";
-}
-
 url = "https://unichatproject.de/";
 wsUrl = "wss://unichatproject.de/";
 
